a5a05f93450e33d53151b94378cf8de527198bb0
felipe <fselmo2@gmail.com>
2026-05-06 15:39:47 -0600

    chore: rm old references to ``devnet`` fork; rm exec fork refs (#710)
    * chore: rm old references to ``devnet`` fork; rm exec fork refs
    
    * fix(test): remaining fork updates ``Devnet`` -> ``Lstar`` for pytest marks
    
    * chore: rename devnet -> lstar

--- tests/consensus/devnet/fc/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Fork choice test vectors for the devnet fork.
-
-This package contains spec test vectors for fork choice behavior.
-"""
---
+++ tests/consensus/lstar/fc/__init__.py
@@ -0,0 +1,5 @@
+"""
+Fork choice test vectors for the lstar fork.
+
+This package contains spec test vectors for fork choice behavior.
+"""
